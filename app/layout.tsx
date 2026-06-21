5c4ded24fd0174af6f4a104b52f0fb5aa65ff8d0
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-06-21 18:46:29 +0600

    pixel id set up done

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pixelId = process.env.NEXT_PUBLIC_FB_PIXEL_ID || "769708002875014";
+
   return (
     <html lang="bn" className={`${poppins.variable}`}>
       <head>
@@ -38,21 +40,25 @@ export default function RootLayout({
           rel="stylesheet"
         />
 
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '769708002875014');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {/* Meta Pixel Code (Next.js Standard Way) */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${pixelId}'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
         {/* End Meta Pixel Code */}
       </head>
       <body
@@ -65,13 +71,12 @@ export default function RootLayout({
         </header>
         <div>{children}</div>
 
-        {/* Meta Pixel Noscript Tag */}
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=769708002875014&ev=PageView&noscript=1"
+            src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
             alt=""
           />
         </noscript>
